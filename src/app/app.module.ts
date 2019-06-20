249e1f97316fad7f4b6c7f39834293ad53fb0d61
Mickael Spennato <mickael.spennato@gmail.com>
2019-06-20 15:49:53 +0200

    création du projet angular + material + akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatOptionModule, MatSelectModule, MatInputModule} from '@angular/material';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatOptionModule, MatSelectModule, MatInputModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
-import { environment } from '../environments/environment'; 
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -28,8 +28,8 @@ import { environment } from '../environments/environment';
     MatSelectModule,
     MatInputModule,
     environment.production ?
-        [] :
-        [ AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot() ]
+      [] :
+      [AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot()]
   ],
   providers: [],
   bootstrap: [AppComponent]
